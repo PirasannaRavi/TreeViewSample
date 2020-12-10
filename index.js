a3447d559eb96940ed37307b497986af0c17d77f
Pirasanna Ravi <pirasanna.ravi@live.com>
2020-12-10 14:04:46 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var DemoApp = angular.module('DemoApp', ['dx']);
+
+DemoApp.controller('DemoController', function DemoController($scope) {
+    $scope.treeViewOptions = {
+        createChildren: function(parent) {
+            var parentId = parent ? parent.itemData.id : "";
+            folderStructure[parentId].push("Test");
+            var fileNames = folderStructure[parentId];
+
+            if (!fileNames) return [];
+
+            return fileNames.map(function(fileName) {
+                var fullName = parentId ? parentId + "\\" + fileName : fileName;
+                return {
+                    id: fullName,
+                    parentId: parentId,
+                    text: fileName,
+                    hasItems: !!folderStructure[fullName]
+                };
+            });
+        },
+        dataStructure: "plain",
+        rootValue: "",
+        height: 500
+    }; 
+});
